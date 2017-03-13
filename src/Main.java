0f72ed0dd36f13cdbef473571e5631e028319ce6
Винникова Анна Игоревна <vinnikovaai.18@sch239.net>
2017-03-13 10:34:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+/**
+ * Created by vinnikovaai.18 on 13.03.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println(a+", "+a*a+", "+a*a*a+", "+a*a*a*a);
+}}
---
+++ src/Main1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/**
+ * Created by vinnikovaai.18 on 13.03.2017.
+ */
+public class Main1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        boolean flag = true;
+        for(int i=2; i<a; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+         if (flag) System.out.println(i);
+            flag=true;
+        }
+    }
+}
+
